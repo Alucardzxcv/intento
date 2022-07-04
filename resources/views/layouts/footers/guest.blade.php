b234db2979f7d215230d077931c7d57cb7b0b527
Alucardzxcv <kevin.carrillo@tecsup.edu.pe>
2022-07-04 01:52:50 -0500

    finalisima

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+
+            </li>
+            <li>
+            <a href="https://creative-tim.com/presentation">
+                {{ __('Acerca de nosotros') }}
+            </a>
+            </li>
+
+            <li>
+            <a href="https://www.creative-tim.com/license">
+                {{ __('Licencias') }}
+            </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, Hecho por <i class="material-icons">favorite</i>
+            <a>Alumnos</a> Tecsup para insentivar la lectura
+        </div>
+    </div>
+</footer>
